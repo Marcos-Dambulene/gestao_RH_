35f5ed5abbfca71ebb5d78088b6369ba1be2f913
Marcos-Dambulene <55637535+Marcos-Dambulene@users.noreply.github.com>
2022-06-17 16:02:55 +0200

    Crud Departamento, Create

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import DepartamentosList
+from .views import DepartamentosList, DepartamentosCreate
 
 
 urlpatterns = [
     path('list', DepartamentosList.as_view(), name='list_departamentos'),
-
+    path('novo', DepartamentosCreate.as_view(), name='create_departamento'),
 ]
